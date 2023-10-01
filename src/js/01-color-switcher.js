6bb9b65497b78cd4d88c4b728460ba50eccb917a
Xrolllex <karolkaczanowski@wp.pl>
2023-10-01 21:08:02 +0200

    commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,9 @@
 
+
+const startSwitcher = document.querySelector("data-start")
+const stopSwitcher = document.querySelector("data-stop")
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  
